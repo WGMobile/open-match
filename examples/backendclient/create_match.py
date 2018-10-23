0bf1a39615c7148347d3590e7fef365aeef299e9
Reik Schatz <r_schatz@wargaming.net>
2018-10-23 12:28:23 +0200

    sending properties

--- examples/backendclient/create_match.py
+++ examples/backendclient/create_match.py
@@ -6,6 +6,15 @@ if __name__ == "__main__":
     channel = grpc.insecure_channel('localhost:50505')
     stub = backend.APIStub(channel)
 
-    profile = Profile(id="foo", properties="")
-    result = stub.CreateMatch(profile)
-    print result
+    profile_name = "testprofile"
+
+    json_data = None
+    with open("profiles/%s.json" % (profile_name,), 'r') as file:        
+        json_data=file.read()
+
+    if json_data:
+        print "creating match for profile: %s" % (profile_name,)
+
+        profile = Profile(id=profile_name, properties=json_data)
+        result = stub.CreateMatch(profile)
+        print result
